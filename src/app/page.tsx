f9d704815fd00868adc6ce333a72ddca36024a71
Saumya Srivastava <saumyasrvstv04@gmail.com>
2025-02-09 22:13:56 +0530

    bg image starting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
-import { LogIn } from "lucide-react";
+import { ArrowRight, LogIn } from "lucide-react";
 import FileUpload from "@/components/FileUpload";
 import { chats } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
@@ -90,33 +90,39 @@ export default async function ChatPage() {
             </h1>
             <UserButton />
           </div>
-
-          {/* Conditional Buttons */}
-          {isAuth && (
-            <Button className="mt-2 px-6 py-3 text-lg shadow-md">
-              Go to Chats
-            </Button>
-          )}
-
-          <p className="max-w-xl text-lg text-white drop-shadow-lg mt-4">
-            Join millions of students, researchers, and professionals to
-            instantly answer questions and understand research with AI.
-          </p>
-
-          {/* File Upload or Login Button */}
-          <div className="w-full mt-4">
-            {isAuth ? (
-              <FileUpload />
-            ) : (
-              <Link href="/sign-in">
-                <center>
-                  <Button className="flex items-center px-6 py-4 text-lg shadow-md">
-                    Login to get Started!
-                    <LogIn className="w-5 h-5 ml-4" />
+          <div className="flex mt-2">
+            {isAuth && firstChat && (
+              <>
+                <Link href={`/chat/${firstChat.id}`}>
+                  <Button>
+                    Go to Chats <ArrowRight className="ml-2" />
                   </Button>
-                </center>
-              </Link>
+                </Link>
+              </>
             )}
+          </div>
+
+          <div className="mt-4">
+            <p className="max-w-xl text-lg text-white drop-shadow-lg mt-4">
+              Join millions of students, researchers, and professionals to
+              instantly answer questions and understand research with AI.
+            </p>
+
+            {/* File Upload or Login Button */}
+            <div className="w-full mt-4">
+              {isAuth ? (
+                <FileUpload />
+              ) : (
+                <Link href="/sign-in">
+                  <center>
+                    <Button className="flex items-center px-6 py-4 text-lg shadow-md">
+                      Login to get Started!
+                      <LogIn className="w-5 h-5 ml-4" />
+                    </Button>
+                  </center>
+                </Link>
+              )}
+            </div>
           </div>
         </div>
       </div>
